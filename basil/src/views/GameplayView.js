98e615529877e4906d231fb17bdea73430542ecc
Duncan Beevers <duncan@dweebd.com>
2013-04-22 19:09:50 -0500

    GameplayView is StackView, has background image

--- basil/src/views/GameplayView.js
+++ basil/src/views/GameplayView.js
@@ -1,14 +1,21 @@
 import ui.ImageView;
+import ui.StackView;
 
-exports = Class(ui.ImageView, function(supr) {
+exports = Class(ui.StackView, function(supr) {
   this.init = function(opts) {
-    opts = merge(opts, {
+    supr(this, "init", [opts]);
+
+    this.setupBackground();
+  };
+
+  this.setupBackground = function() {
+    var backgroundImageView = new ui.ImageView({
+      superview: this,
       x: 0,
       y: 0,
-      autoSize: true,
+      width: 1,
+      height: 1,
       image: "resources/images/128x128_thumb.png"
     });
-
-    supr(this, "init", [opts]);
-  };
+  }
 });
