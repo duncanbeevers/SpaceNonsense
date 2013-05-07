2c778d787b7255f2ae5005e312629ce66dfc5f25
Duncan Beevers <duncan@dweebd.com>
2013-05-07 13:54:52 -0500

    AsteroidGenerator tracks&returns furthest asteroid

--- basil/src/AsteroidGenerator.js
+++ basil/src/AsteroidGenerator.js
@@ -1,5 +1,6 @@
 import src.models.Asteroid as Asteroid;
 
+import src.lib.FW_Math as FW.Math;
 import src.lib.Box2dWeb_2_1_a_3 as Box2D;
 
 exports = Class(function(supr) {
@@ -10,6 +11,8 @@ exports = Class(function(supr) {
 
     // immediately
     this.nextAsteroidIn = 0; // ms
+
+    this.asteroids = [];
   };
 
   this.processTime = function(dt) {
@@ -22,5 +25,34 @@ exports = Class(function(supr) {
 
   this.spawnAsteroid = function() {
     var asteroid = new Asteroid(this.player, this.world, this.superview);
+    this.asteroids.push(asteroid);
+  };
+
+  this.furthestAsteroidDistance = function() {
+    return this.furthestAsteroidAndDistance()['distance'];
+  };
+
+  this.furthestAsteroidAndDistance = function() {
+    var asteroids = this.asteroids,
+        asteroid,
+        asteroidPosition,
+        playerPosition = this.player.getPosition(),
+        playerX = playerPosition.x,
+        playerY = playerPosition.y,
+        result = { asteroid: null, distance: null },
+        distance;
+
+    // Maximize distance, memoize calculation result and element
+    for (var i = 0; i < asteroids.length; i++) {
+      asteroid = asteroids[i];
+      asteroidPosition = asteroid.getPosition();
+      distance = FW.Math.distance(asteroidPosition.x, asteroidPosition.y, playerX, playerY);
+      if (distance > result['distance']) {
+        result['distance'] = distance;
+        result['asteroid'] = asteroid;
+      }
+    }
+
+    return result;
   };
 });
