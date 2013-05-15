ad26f2df1cafe9c93b7493bb9ab216a7b49d7597
Duncan Beevers <duncan@dweebd.com>
2013-05-14 22:59:23 -0500

    Add getWidth and getHeight to FW_GCDevice

--- basil/src/lib/FW_GameClosureDevice.js
+++ basil/src/lib/FW_GameClosureDevice.js
@@ -12,7 +12,9 @@ function getMinDimension() {
 
 FW.GameClosureDevice = {
   getMaxDimension: getMaxDimension,
-  getMinDimension: getMinDimension
+  getMinDimension: getMinDimension,
+  getWidth: function() { return device.width; },
+  getHeight: function() { return device.height; }
 };
 
 exports = FW.GameClosureDevice;
