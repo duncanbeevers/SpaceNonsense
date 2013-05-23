9d7466780f010838d6f0655f239ace7967e80d7d
Duncan Beevers <duncan@dweebd.com>
2013-05-23 14:47:48 -0500

    Bullet is damageable

--- basil/src/models/Bullet/Bullet.js
+++ basil/src/models/Bullet/Bullet.js
@@ -1,6 +1,8 @@
 import src.models.Bullet.BulletView as BulletView;
 import src.models.Bullet.BulletPhysics as BulletPhysics;
 
+import src.lib.FW_Dispatcher as FW.Dispatcher;
+import src.lib.FW_DamageableMixin as FW.DamageableMixin;
 import src.lib.FW_GameClosureExtend as FW.GameClosureExtend;
 import src.lib.FW_GameClosurePhysicsViewSyncMixin as FW.GameClosurePhysicsViewSyncMixin;
 
@@ -8,6 +10,7 @@ exports = Class(function(supr) {
   this.init = function(gameDispatcher, bulletName, trajectory, world, superview, x, y) {
     this.name = bulletName;
     this.lifespan = 10000;
+    this.maxLife = 500;
 
     // TODO: Look up bullet size?
     var radius = 0.6;
@@ -21,6 +24,13 @@ exports = Class(function(supr) {
       this.countdownLifespan(dt);
     }, this);
 
+    var dispatcher = new FW.Dispatcher();
+
+    this.dispatcher = dispatcher;
+
+    // Bullets "take damage" when they bump into Asteroids
+    // When enough damage is taken, the bullet expires immediately
+    this.onDied(this.die);
   };
 
   this.countdownLifespan = function(dt) {
@@ -34,5 +44,6 @@ exports = Class(function(supr) {
     this.gameDispatcher.offByBindTarget("tick", this);
   };
 
+  FW.GameClosureExtend(this, FW.DamageableMixin);
   FW.GameClosureExtend(this, FW.GameClosurePhysicsViewSyncMixin);
 });
