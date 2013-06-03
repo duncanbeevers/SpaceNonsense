26c7cd63af3ea6ccfde2295850ebaa7b1702545f
Duncan Beevers <duncan@dweebd.com>
2013-06-03 06:33:05 -0500

    Bullet generation moved out of base Player class

--- basil/src/models/BasePlayer.js
+++ basil/src/models/BasePlayer.js
@@ -1,6 +1,5 @@
 import src.models.Player.PlayerView as PlayerView;
 import src.models.Player.PlayerPhysics as PlayerPhysics;
-import src.models.Bullet.Bullet as Bullet;
 
 import src.lib.FW_GameClosureExtend as FW.GameClosureExtend;
 import src.lib.FW_GameClosurePhysicsViewSyncMixin as FW.GameClosurePhysicsViewSyncMixin;
@@ -11,53 +10,18 @@ exports = Class(function(supr) {
   this.init = function(gameDispatcher, world, superview) {
     this.radius = 1;
 
-    this.weapons = [
-      { image: "Bullet001",
-        cooldown: 200
-      }
-    ];
-    this.currentWeaponIndex = 0;
-
     this.gameDispatcher = gameDispatcher;
     this.view = new PlayerView(this, { superview: superview });
     this.physics = new PlayerPhysics(this, 0, -3, this.getRadius(), world);
-
-    gameDispatcher.onTick(this.cooldownWeapons, this);
   };
 
   this.shoot = function(dt) {
-    var weapon = this.weapons[this.currentWeaponIndex];
-    if (!weapon.cooldownRemaining) {
-      weapon.cooldownRemaining = weapon.cooldown;
-
-      var playerPosition = this.getPosition(),
-          trajectory = playerPosition.r,
-          bulletSize = this.getRadius() / 5,
-          bulletDistance = this.getRadius() + bulletSize;
-
-      // TODO: Recycle Bullet objects
-      new Bullet(this.gameDispatcher, bulletSize, weapon.image, trajectory, this.physics.getWorld(), this.view.getSuperview(),
-        playerPosition.x + Math.cos(trajectory) * bulletDistance,
-        playerPosition.y + Math.sin(trajectory) * bulletDistance
-      );
-
-    }
   };
 
   this.reward = function(size) {
     this.radius += size;
     this.physics.setRadius(this.radius);
     this.view.scaleAndCenter();
-  };
-
-  this.cooldownWeapons = function(dt) {
-    var weapon;
-    for (var i = this.weapons.length - 1; i >= 0; i--) {
-      weapon = this.weapons[i];
-      if (weapon.cooldownRemaining) {
-        weapon.cooldownRemaining -= Math.min(weapon.cooldownRemaining, dt);
-      }
-    }
   };
 
   this.pointAt = function(angle) {
--- basil/src/scenarios/Asteroids/Player.js
+++ basil/src/scenarios/Asteroids/Player.js
@@ -1,4 +1,48 @@
 import src.models.BasePlayer as BasePlayer;
+import src.models.Bullet.Bullet as Bullet;
 
 exports = Class(BasePlayer, function(supr) {
+  this.init = function(gameDispatcher, world, superview) {
+    supr(this, "init", arguments);
+
+    gameDispatcher.onTick(this.cooldownWeapons, this);
+
+    this.weapons = [
+      { image: "Bullet001",
+        cooldown: 200
+      }
+    ];
+    this.currentWeaponIndex = 0;
+  };
+
+  this.cooldownWeapons = function(dt) {
+    var weapon;
+    for (var i = this.weapons.length - 1; i >= 0; i--) {
+      weapon = this.weapons[i];
+      if (weapon.cooldownRemaining) {
+        weapon.cooldownRemaining -= Math.min(weapon.cooldownRemaining, dt);
+      }
+    }
+  };
+
+
+  this.shoot = function(dt) {
+    var weapon = this.weapons[this.currentWeaponIndex];
+    if (!weapon.cooldownRemaining) {
+      weapon.cooldownRemaining = weapon.cooldown;
+
+      var playerPosition = this.getPosition(),
+          trajectory = playerPosition.r,
+          bulletSize = this.getRadius() / 5,
+          bulletDistance = this.getRadius() + bulletSize;
+
+      // TODO: Recycle Bullet objects
+      new Bullet(this.gameDispatcher, bulletSize, weapon.image, trajectory, this.physics.getWorld(), this.view.getSuperview(),
+        playerPosition.x + Math.cos(trajectory) * bulletDistance,
+        playerPosition.y + Math.sin(trajectory) * bulletDistance
+      );
+
+    }
+  };
+
 });
